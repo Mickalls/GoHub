b8079bb3c17f4b134a8ea3f13f253f157a8805ea
wangyouhua <20222131060@m.scnu.edu.cn>
2024-10-27 19:43:19 +0800

    初始化v1测试路由组

--- main.go
+++ main.go
@@ -1,38 +1,19 @@
 package main
 
 import (
+	"GoHub/bootstrap"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strings"
 )
 
 func main() {
-	// 初始化 Gin 实例
-	r := gin.New()
-
-	// 注册中间件
-	r.Use(gin.Logger(), gin.Recovery())
-
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World!",
-		})
-	})
-
-	r.NoRoute(func(c *gin.Context) {
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// 如果是 HTML 的话
-			c.String(http.StatusNotFound, "页面返回: 404")
-		} else {
-			// 默认返回 JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认url和请求方法是否正确",
-			})
-		}
-	})
-
-	r.Run(":8000")
+	// new 一个 gin engine 实例
+	router := gin.New()
+	// 初始化绑定路由
+	bootstrap.SetUpRoute(router)
+	// 运行服务
+	if err := router.Run(":3000"); err != nil {
+		// 错误处理
+		fmt.Println(err.Error())
+	}
 }
